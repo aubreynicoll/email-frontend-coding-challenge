108fa46039b2e8be9d344e1e10a2bcb6e8df8cee
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-02-10 15:00:48 +1000

    import mock emails

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import emailData from './emails.json'
 
-const App = () => (
-  <h1>Hello World!</h1>
-)
+const App = () => {
+  const [emailList, setEmailList] = useState([])
+
+  useEffect(() => {
+    const emails = emailData.map((email) => ({ ...email, date: new Date(email.date) }))
+    setEmailList(emails)
+  }, [])
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <ul>
+        {emailList.map((email) => (
+          <li key={email.id}>
+            {email.id}
+            {' '}
+            {email.date.toString()}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default App
