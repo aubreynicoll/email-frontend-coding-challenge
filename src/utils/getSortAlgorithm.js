be1abd69a3357f6b6ca4fed326711f288dd4f53c
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-02-12 15:03:30 +1000

    sort based on lowercase values

--- src/utils/getSortAlgorithm.js
+++ src/utils/getSortAlgorithm.js
@@ -1,10 +1,23 @@
 // key must be one of Object.keys(email)
 
-const getSortAlgorithm = ({ key, isAscending }) => (a, b) => {
-  if (a[key] > b[key]) {
-    return isAscending ? 1 : -1
+const getSortAlgorithm = ({ key, isAscending }) => {
+  switch (key) {
+    case 'date':
+      return ((a, b) => {
+        if (a[key] > b[key]) {
+          return isAscending ? 1 : -1
+        }
+        return isAscending ? -1 : 1
+      })
+
+    default:
+      return ((a, b) => {
+        if (a[key].toLowerCase() > b[key].toLowerCase()) {
+          return isAscending ? 1 : -1
+        }
+        return isAscending ? -1 : 1
+      })
   }
-  return isAscending ? -1 : 1
 }
 
 export default getSortAlgorithm
