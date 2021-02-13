8ba0742736509212c450d687cd149bb9011fa5ee
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-02-13 14:47:26 +1000

    style MobileEmailList

--- src/components/MobileEmailList.js
+++ src/components/MobileEmailList.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable arrow-body-style */
@@ -5,8 +7,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useHistory } from 'react-router-dom'
 import { ReactComponent as ArrowIcon } from '../assets/icons/icon_arrow01.svg'
+import { ReactComponent as ArrowIcon02 } from '../assets/icons/icon_arrow02.svg'
 import { ReactComponent as AttachmentIcon } from '../assets/icons/icon_clip.svg'
 import formatDate from '../utils/formatDate'
+import { ReactComponent as MailIcon } from '../assets/icons/icon_mail_sp.svg'
 
 const MobileEmailList = ({
   emailList, getSortColStyle, getSortArrowStyle, handleSortKeyChange,
@@ -14,7 +18,80 @@ const MobileEmailList = ({
   const history = useHistory()
   return (
     <div className="MobileEmailList-root MobileDisplay">
-      <h1>Mobile View!</h1>
+      <div className="MobileEmailList-sortBar">
+        <div
+          style={getSortColStyle('from')}
+          onClick={() => handleSortKeyChange('from')}
+        >
+          <span>From</span>
+          <ArrowIcon style={getSortArrowStyle('from')} />
+        </div>
+        {' | '}
+        <div
+          style={getSortColStyle('to')}
+          onClick={() => handleSortKeyChange('to')}
+        >
+          <span>To</span>
+          <ArrowIcon style={getSortArrowStyle('to')} />
+        </div>
+        {' | '}
+        <div
+          style={getSortColStyle('subject')}
+          onClick={() => handleSortKeyChange('subject')}
+        >
+          <span>Subject</span>
+          <ArrowIcon style={getSortArrowStyle('subject')} />
+        </div>
+        {' | '}
+        <div
+          style={getSortColStyle('date')}
+          onClick={() => handleSortKeyChange('date')}
+        >
+          <span>Date</span>
+          <ArrowIcon style={getSortArrowStyle('date')} />
+        </div>
+      </div>
+
+      <div className="MobileEmailList-body">
+        {emailList.map((email) => (
+          <div className="ListCard-root">
+            <div className="MailIcon-row-container">
+
+              <div className="MailIcon-div">
+                <MailIcon width="16" />
+              </div>
+
+              <div className="EmailAddresses-col-container">
+                <div className="EmailFrom-row-container">
+
+                  <div className="EmailFrom-div text email">
+                    {email.from}
+                  </div>
+
+                  <div className="Date-row-container">
+                    <AttachmentIcon width="16" />
+                    <div className="Date-div">
+                      {formatDate(email.date)}
+                    </div>
+                    <ArrowIcon02 width="4" />
+                  </div>
+
+                </div>
+
+                <div className="EmailTo-div text email">
+                  {email.to}
+                </div>
+              </div>
+
+            </div>
+
+            <div className="EmailSubject-div text subject">
+              {email.subject}
+            </div>
+          </div>
+        ))}
+
+      </div>
     </div>
   )
 }
